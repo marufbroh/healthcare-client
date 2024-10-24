d34f0b48c3cb4ff1b0119c2a25f09e76e1cddf1c
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-10-24 12:33:29 +0600

    Start Developing Specialist Feature in Admin Dashboard

--- src/app/(withDashboardLayout)/dashboard/admin/specialties/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/specialties/page.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import HModal from "@/components/Shared/HModal/HModal";
+import { Box, Button, Stack, TextField } from "@mui/material";
+import React from "react";
 
 const SpecialtiesPage = () => {
-    return (
-        <div>
-            <h1>Specialties page</h1>
-        </div>
-    );
+  return (
+    <Box>
+      <Stack
+        direction="row"
+        justifyContent="space-between"
+        alignItems={"center"}
+      >
+        <Button>Create Specialty</Button>
+        <HModal/>
+        <TextField size="small" placeholder="Search Specialties" />
+      </Stack>
+    </Box>
+  );
 };
 
 export default SpecialtiesPage;
