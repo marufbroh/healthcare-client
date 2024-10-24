4a9652981679d74ad737b30a4b9a718b699655e5
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-10-24 16:51:06 +0600

    Displaying Specialty Data Using Data Grid Table

--- src/app/(withDashboardLayout)/dashboard/admin/specialties/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/specialties/page.tsx
@@ -1,18 +1,27 @@
 "use client";
 
-import { Box, Button, Stack, TextField } from "@mui/material";
+import { useGetAllSpecialtiesQuery } from "@/redux/api/specialtiesApi";
+import {
+  Box,
+  Button,
+  LinearProgress,
+  Paper,
+  Stack,
+  TextField,
+} from "@mui/material";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import { useState } from "react";
 import SpecialtyModal from "./components/SpecialtyModal";
-import { useGetAllSpecialtiesQuery } from "@/redux/api/specialtiesApi";
 
 const SpecialtiesPage = () => {
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
-  const {data: allSpecialties, isLoading} = useGetAllSpecialtiesQuery({});
+  const { data: allSpecialties, isLoading } = useGetAllSpecialtiesQuery({});
 
-  if(isLoading){
+  const columns: GridColDef[] = [
+    { field: "title", headerName: "Title", width: 100 },
+    { field: "icon", headerName: "Icon", width: 100, renderCell: ({row}) => () },
+  ];
 
-  };
-  
   return (
     <Box>
       <Stack
@@ -25,8 +34,19 @@ const SpecialtiesPage = () => {
         <TextField size="small" placeholder="Search Specialties" />
       </Stack>
 
-
-
+      {!isLoading ? (
+        <Paper sx={{ height: 400, width: "100%" }}>
+          <DataGrid
+            rows={allSpecialties}
+            columns={columns}
+            sx={{ border: 0 }}
+          />
+        </Paper>
+      ) : (
+        <Box sx={{ width: "100%", my: "10px" }}>
+          <LinearProgress />
+        </Box>
+      )}
     </Box>
   );
 };
