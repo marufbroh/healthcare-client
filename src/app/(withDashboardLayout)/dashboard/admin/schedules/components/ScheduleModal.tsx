a0cf4b8dec353d353f3a691494c30ef6a4207c9d
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-10-26 20:56:10 +0600

    Start Developing Specialist Feature in Admin Dashboard

---
+++ src/app/(withDashboardLayout)/dashboard/admin/schedules/components/ScheduleModal.tsx
@@ -0,0 +1,51 @@
+import HDatePickers from "@/components/Forms/HDatePickers";
+import HForm from "@/components/Forms/HForm";
+import HModal from "@/components/Shared/HModal/HModal";
+import { modifyPayload } from "@/utils/modifyPayload";
+import { Button, Grid2 } from "@mui/material";
+import { FieldValues } from "react-hook-form";
+import { toast } from "sonner";
+
+type TProps = {
+    open: boolean;
+    setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  };
+
+const ScheduleModal = ({ open, setOpen }: TProps) => {
+
+
+    const handleFormSubmit = async (values: FieldValues) => {
+        const data = modifyPayload(values);
+    
+        try {
+        //   const res = await createSpecialty(data).unwrap();
+        //   if (res?.id) {
+        //     toast.success("Specialty created successfully");
+        //     setOpen(false);
+        //   }
+        } catch (error: any) {
+          console.error(error.message);
+        }
+      };
+    
+      return (
+        <HModal open={open} setOpen={setOpen} title={"Create A New Specialty"}>
+          <HForm onSubmit={handleFormSubmit}>
+            <Grid2 container spacing={2}>
+              <Grid2 size={{ md: 12 }}>
+               <HDatePickers/>
+              </Grid2>
+              <Grid2 size={{ md:12 }}>
+                
+              </Grid2>
+            </Grid2>
+    
+            <Button type="submit" fullWidth sx={{ my: 1 }}>
+              Create
+            </Button>
+          </HForm>
+        </HModal>
+      );
+};
+
+export default ScheduleModal;
--- src/app/(withDashboardLayout)/dashboard/admin/schedules/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/schedules/page.tsx
@@ -1,10 +1,38 @@
-import React from 'react';
+"use client";
+
+import { Box, Button, Stack, TextField } from '@mui/material';
+import React, { useState } from 'react';
+import ScheduleModal from './components/ScheduleModal';
 
 const SchedulesPage = () => {
+    const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
     return (
-        <div>
-            schedules
-        </div>
+        <Box>
+      <Stack
+        direction="row"
+        justifyContent="space-between"
+        alignItems={"center"}
+      >
+        <Button onClick={() => setIsModalOpen(true)}>Create Schedule</Button>
+        <ScheduleModal open={isModalOpen} setOpen={setIsModalOpen} />
+        <TextField size="small" placeholder="Search Schedule" />
+      </Stack>
+
+      {/* {!isLoading ? (
+        <Paper sx={{ height: "100%", width: "100%", my: "16px" }}>
+          <DataGrid
+            rows={allSpecialties}
+            columns={columns}
+            sx={{ border: 0 }}
+            hideFooter={true}
+          />
+        </Paper>
+      ) : (
+        <Box sx={{ width: "100%", my: "16px" }}>
+          <LinearProgress />
+        </Box>
+      )} */}
+    </Box>
     );
 };
 
