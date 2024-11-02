0aabd0870ac5c843996e5631498ce10ce175c012
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-11-02 08:29:58 +0600

    Handling Logout with Removing Cookies

--- src/components/UI/AuthButton/AuthButton.tsx
+++ src/components/UI/AuthButton/AuthButton.tsx
@@ -1,19 +1,20 @@
 "use client";
 
-import { getUserInfo, removeUser } from "@/services/auth.services";
+import { logoutUser } from "@/services/actions/logoutUser";
+import { getUserInfo } from "@/services/auth.services";
 import { Button } from "@mui/material";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 const AuthButton = () => {
-    const router = useRouter();
-    const userInfo = getUserInfo();
-    // console.log({userInfo});
-  
-    const handleLogout = () => {
-      removeUser();
-      router.refresh();
-    };
+  const router = useRouter();
+  const userInfo = getUserInfo();
+  // console.log({userInfo});
+
+  const handleLogout = () => {
+    logoutUser(router);
+  };
+
   return (
     <>
       {userInfo?.userId ? (
