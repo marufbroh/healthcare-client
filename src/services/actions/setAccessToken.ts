d8d52dcb0503289337052a21726c2f03cf1c9813
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-11-04 09:22:57 +0600

    Implementing Reset Password and Login Redirection For Patient

--- src/services/actions/setAccessToken.ts
+++ src/services/actions/setAccessToken.ts
@@ -1,11 +1,17 @@
 "use server"
 
-import { cookies } from 'next/headers'
 import { authKey } from "@/constant/authKey";
+import { decodedToken } from '@/utils/jwt';
+import { cookies } from 'next/headers';
 import { redirect } from "next/navigation";
 
 const setAccessToken = (token: string, option?: any) => {
     cookies().set(authKey, token);
+    const decodedData = decodedToken(token) as any;
+
+    if (option && decodedData?.role === "PATIENT" && option.redirect) {
+        redirect(option.redirect);
+    }
     if (option && option.passwordChangeRequired) {
         redirect('/dashboard/change-password');
     }
